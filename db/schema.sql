3400747f0a294dd793c786577961861bed44beb2
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-14 15:53:30 -0500

    adds .env file

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,7 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+source ./seeds/category-seeds.js;
+source ./seeds/product-seeds.js;
+source ./seeds/product-tag-seeds.js;
+source ./seeds/tag-seeds.js;
