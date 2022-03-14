9425da302006a45b8df7d53fdabfefccfba3d080
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-14 13:28:46 -0500

    updates Products model

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,49 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
+Product.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    validate: {
+      isDecimal: true,
+    }
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    },
+    references: {
+      type: DataTypes.INTEGER,
+      model: 'Category',
+      key: 'id'
+    }
   }
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "product",
+}
 );
 
 module.exports = Product;
